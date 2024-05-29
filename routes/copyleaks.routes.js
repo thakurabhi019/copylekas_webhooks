632e0ed148c6a0126643b24d67db5d3d820d1363
Abhi <abhishekloharli19@gmail.com>
2024-05-29 17:29:00 +0530

    logged body of webhook

--- routes/copyleaks.routes.js
+++ routes/copyleaks.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 router.post('/copyleaks/:status/:scanId',(req,res) => {
     const {status,scanId} = req.params;
-    console.log({status,scanId})
+    console.log({status,scanId});
+    const body = req.body;
+    console.log(body);
     res.send(`Webhook called for scanId: ${scanId} and STATUS: ${status}`);
 })
 
