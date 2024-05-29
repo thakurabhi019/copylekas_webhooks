c8ebe335709a337ba95eaed600529ae56687bd65
Abhi <abhishekloharli19@gmail.com>
2024-05-29 17:43:04 +0530

    added export webhook

--- routes/copyleaks.routes.js
+++ routes/copyleaks.routes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/export/:export_id/results/:result_id',(req,res) => {
+    console.log('export webhook called');
+    const {export_id,result_id} = req.params;
+    console.log({export_id,result_id});
+    const body = req.body;
+    console.log(JSON.stringify(body));
+    res.send(`Webhook called for export_id: ${export_id} and result_id: ${result_id}`);
+})
+
 router.post('/copyleaks/:status/:scanId',(req,res) => {
+    console.log('status webhook called')
     const {status,scanId} = req.params;
     console.log({status,scanId});
-    const body = req.body;
-    console.log(body);
+    const {results} = req.body;
+    console.log(JSON.stringify(results));
     res.send(`Webhook called for scanId: ${scanId} and STATUS: ${status}`);
 })
 
