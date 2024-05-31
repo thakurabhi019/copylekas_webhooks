fba562c235b37ca615e503c08ff657a0a6c7cb61
Gaurav <gk44035@gmail.com>
2024-05-31 15:27:45 +0530

    test

--- routes/copyleaks.routes.js
+++ routes/copyleaks.routes.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.post('/export/:export_id/results/:result_id',(req,res) => {
-    console.log('export webhook called');
-    const {export_id,result_id} = req.params;
-    console.log({export_id,result_id});
-    const body = req.body;
-    console.log(JSON.stringify(body));
-    res.send(`Webhook called for export_id: ${export_id} and result_id: ${result_id}`);
-})
+router.post("/export/:export_id/results/:result_id", (req, res) => {
+  console.log("export webhook called");
+  const { export_id, result_id } = req.params;
+  console.log({ export_id, result_id });
+  const body = req.body;
+  console.log(JSON.stringify(body));
+  console.log("abcd");
+  res.send(
+    `Webhook called for export_id: ${export_id} and result_id: ${result_id}`
+  );
+});
 
-router.post('/copyleaks/:status/:scanId',(req,res) => {
-    console.log('status webhook called')
-    const {status,scanId} = req.params;
-    console.log({status,scanId});
-    const {results} = req.body;
-    console.log(JSON.stringify(results));
-    res.send(`Webhook called for scanId: ${scanId} and STATUS: ${status}`);
-})
+router.post("/copyleaks/:status/:scanId", (req, res) => {
+  console.log("status webhook called");
+  const { status, scanId } = req.params;
+  console.log({ status, scanId });
+  const { results } = req.body;
+  console.log(JSON.stringify(results));
+  res.send(`Webhook called for scanId: ${scanId} and STATUS: ${status}`);
+});
 
-module.exports = router
+module.exports = router;
