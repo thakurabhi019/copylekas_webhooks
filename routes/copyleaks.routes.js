9f2871a8d3d43f990f830a2b84124823dea98752
Abhi <abhishekloharli19@gmail.com>
2024-06-10 17:27:48 +0530

    pdf report

--- routes/copyleaks.routes.js
+++ routes/copyleaks.routes.js
@@ -19,4 +19,17 @@ router.post('/copyleaks/:status/:scanId',(req,res) => {
     res.send(`Webhook called for scanId: ${scanId} and STATUS: ${status}`);
 })
 
+// pdf report
+// "endpoint": "https://copylekas-webhooks.onrender.com/copyleaks/export/sitaram1234567/pdf-report"
+router.post("/copyleaks/export/:export_id/pdf-report", (req, res) => {
+    console.log("result pdf webhook called");
+    // const { export_id, result_id } = req.params;
+    // console.log({ export_id, result_id });
+    const body = req.body;
+    console.log(JSON.stringify(body));
+    res.send(
+      `Webhook called for export_id pdf`
+    );
+  });
+
 module.exports = router
