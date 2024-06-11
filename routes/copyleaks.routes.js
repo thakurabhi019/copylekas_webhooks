359d7b36aaf56f603e69f43d990e9ac011ba83e7
Abhishek Thakur <149044917+thakurabhi019@users.noreply.github.com>
2024-06-11 10:33:39 +0530

    Update copyleaks.routes.js

--- routes/copyleaks.routes.js
+++ routes/copyleaks.routes.js
@@ -26,7 +26,7 @@ router.post("/copyleaks/export/:export_id/results/:result_id", (req, res) => {
 // pdf report
 // "endpoint": "https://copylekas-webhooks.onrender.com/copyleaks/export/sitaram1234567/pdf-report"
 router.post("/copyleaks/export/:export_id/pdf-report", (req, res) => {
-    console.log("result pdf webhook called");
+    console.log("result pdf webhook called==============================");
     // const { export_id, result_id } = req.params;
     // console.log({ export_id, result_id });
     const body = req.body;
