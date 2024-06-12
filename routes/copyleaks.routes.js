9d97dbf67afd29ae02c1b4f8ca77662e59bf4278
Abhishek Thakur <149044917+thakurabhi019@users.noreply.github.com>
2024-06-12 16:23:14 +0530

    Update copyleaks.routes.js

--- routes/copyleaks.routes.js
+++ routes/copyleaks.routes.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+// pdf report
+// "endpoint": "https://copylekas-webhooks.onrender.com/copyleaks/export/sitaram1234567/pdf-report"
+router.post("/copyleaks/export/:export_id/pdf-report", (req, res) => {
+    console.log("result pdf webhook called==============================");
+    // const { export_id, result_id } = req.params;
+    // console.log({ export_id, result_id });
+    const body = req.body;
+    console.log(JSON.stringify(body));
+    res.send(
+      `Webhook called for export_id pdf`
+    );
+  });
+
 router.post("/copyleaks/export/:export_id/:status", (req, res) => {
   console.log("export completed webhook called");
   const { export_id, status } = req.params;
@@ -23,20 +36,10 @@ router.post("/copyleaks/export/:export_id/results/:result_id", (req, res) => {
   );
 });
 
-// pdf report
-// "endpoint": "https://copylekas-webhooks.onrender.com/copyleaks/export/sitaram1234567/pdf-report"
-router.post("/copyleaks/export/:export_id/pdf-report", (req, res) => {
-    console.log("result pdf webhook called==============================");
-    // const { export_id, result_id } = req.params;
-    // console.log({ export_id, result_id });
-    const body = req.body;
-    console.log(JSON.stringify(body));
-    res.send(
-      `Webhook called for export_id pdf`
-    );
-  });
 
-module.exports = router
+
+
+// module.exports = router
 router.post("/copyleaks/:status/:scanId", (req, res) => {
   console.log("status webhook called");
   const { status, scanId } = req.params;
